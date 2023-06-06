e5d1b2e33428f022fb52defe4e12371e934b43bf
Serzat Demirer <91182016+SerzatDemirer@users.noreply.github.com>
2023-06-06 22:59:22 +0200

    feat: Game logic is constructed & css added

--- index.js
+++ index.js
@@ -12,19 +12,45 @@ console.log(document.querySelector('.guess').value);
  */
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //WHEN THERE IS NO INPUT
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
+
+    //WHEN PLAYER WINS
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //WHEN GUESS IS TOO HIGH
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too high!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    //WHEN GUESS IS TOO LOW
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too low!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
